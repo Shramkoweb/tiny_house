99dca633e4f86b5507460c9d678f1b28e4403208
Serhii Shramko <shramko.web@yahoo.com>
2022-06-12 14:25:08 +0200

    chore(build): add ts and config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from 'express';
 
 const PORT = 9000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get('/', (req, res) => res.send('123 world'));
 
 app.listen(PORT, () => console.log(`[app] : http://localhost:${PORT}`));
 
