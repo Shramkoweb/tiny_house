435eb21620ca1e230a54813307e1460d063b7131
Serhii Shramko <shramko.web@yahoo.com>
2022-07-03 18:38:20 +0200

    Faet/15 add ant design (#16)
    * build(deps): rm unexcited package @types/graphql
    
    * build(deps): add antd
    
    * build(deps): rm unexcited package from server
    
    * feat: add listings render functionality

--- client/src/sections/listings/listings.tsx
+++ client/src/sections/listings/listings.tsx
@@ -1,13 +1,19 @@
 import React, {MouseEvent} from 'react';
 import {useQuery, useMutation} from '@apollo/client';
+import {List, Avatar, Button, Spin, Alert} from 'antd';
 
 import {
-  ListingsDocument,
   DeleteListingDocument,
-  DeleteListingMutationVariables,
   DeleteListingMutation,
+  DeleteListingMutationVariables,
+  ListingsDocument,
   ListingsQuery,
-} from '../../../graphql/generated';
+} from '../../generated';
+
+// TODO add import order to eslint
+import './styles/index.css';
+
+import {ListingsSkeleton} from './components/listing-skeleton';
 
 interface ListingsProps {
   title: string;
@@ -16,10 +22,11 @@ interface ListingsProps {
 export const Listings = (props: ListingsProps) => {
   const {title} = props;
 
-
   const {error, data, refetch, loading} = useQuery<ListingsQuery>(ListingsDocument);
-  const [deleteListing] = useMutation<DeleteListingMutation, DeleteListingMutationVariables>(DeleteListingDocument);
-
+  const [deleteListing, {
+    loading: deleteListingLoading,
+    error: deleteListingError,
+  }] = useMutation<DeleteListingMutation, DeleteListingMutationVariables>(DeleteListingDocument);
   const handleDeleteListing = (evt: MouseEvent<HTMLButtonElement>) => {
     const fetchDelete = async () => {
       await deleteListing({variables: {id: evt.currentTarget.id}});
@@ -29,21 +36,47 @@ export const Listings = (props: ListingsProps) => {
     fetchDelete();
   };
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) {
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title}/>
+      </div>
+    );
+  }
 
-  return <>
-    <h2>{title}</h2>
-    {data?.listings && (
-      <ul>
-        {data?.listings.map(listing => {
-          return (
-            <li key={listing.id}>
-              {listing.title}{' '}
-              <button id={listing.id} onClick={handleDeleteListing}>Delete</button>
-            </li>
-          );
-        })}
-      </ul>)}
-  </>;
+  if (error) {
+    return (
+      <div className="listings">
+        <ListingsSkeleton title={title} error/>
+      </div>
+    );
+  }
+
+  // TODO refactor to won component -> see listing-skeleton.tsx
+  const deleteListingErrorAlert = deleteListingError ? (
+    <Alert
+      type="error"
+      message="Uh oh! Something went wrong :(. Please try again later."
+      className="listings__alert"
+    />
+  ) : null;
+
+  return <div className="listings">
+    {deleteListingErrorAlert}
+
+    <Spin spinning={deleteListingLoading}>
+      <h2>{title}</h2>
+      {data?.listings && (
+        <List itemLayout="horizontal" dataSource={data.listings} renderItem={listing => (
+          <List.Item actions={[<Button type="primary" id={listing.id} onClick={handleDeleteListing}>Delete</Button>]}>
+            <List.Item.Meta
+              title={listing.title}
+              description={listing.address}
+              avatar={<Avatar src={listing.image} size={48} shape="square"/>}
+            />
+          </List.Item>
+        )}>
+        </List>)}
+    </Spin>
+  </div>;
 };
