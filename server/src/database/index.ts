77275146998d843bc1fc07e98a8f0d0b0d802c60
Serhii Shramko <shramko.web@yahoo.com>
2022-07-13 22:10:10 +0200

    feat: add database document structure

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,6 +1,6 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-import {Database} from '../lib/types';
+import { Booking, Database, Listing, User } from '../lib/types';
 
 const CONNECT_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.spuif.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -9,7 +9,9 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db('main');
 
   return {
-    listings: db.collection('test_listings'),
+    bookings: db.collection<Booking>('bookings'),
+    listings: db.collection<Listing>('listings'),
+    users: db.collection<User>('users'),
   };
 };
 
