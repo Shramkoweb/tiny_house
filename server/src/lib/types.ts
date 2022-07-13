5cc3d7049759fbba6034b82397c4356b70e4236c
Serhii Shramko <shramko.web@yahoo.com>
2022-07-13 22:41:48 +0200

    feat: add database document structure (#21)
    * feat: add database document structure
    
    * feat: add database document structure

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,17 +1,66 @@
-import {ObjectId, Collection} from 'mongodb';
+import { ObjectId, Collection } from 'mongodb';
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+
+export enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE'
+}
+
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
+}
 
 export interface Listing {
   _id: ObjectId;
-  title: string;
-  image: string;
-  address: string;
   price: number;
-  numOfGuests: number;
   numOfBeds: number;
-  numOfBaths: number;
   rating: number;
+  numOfBaths: number;
+  numOfStars: number;
+  numOfVotes: number;
+  numOfGuests: number;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  token: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
